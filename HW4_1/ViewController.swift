b819f002cbed24521474d36c7f2d69ba1c31143e
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-02-06 20:23:45 +0100

    Initial Commit

---
+++ HW4_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4_1
+//
+//  Created by Nikita Zharinov on 06/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
